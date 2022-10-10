42a4e57dc25d391c29b23f3db311817385bb9fa7
Kakhi <kakhi_varazashvili@yahoo.it>
2022-10-10 16:25:30 +0400

    check passwords

--- local1.js
+++ local1.js
@@ -3,16 +3,43 @@ clear.addEventListener("click", function(){
     localStorage.clear()
 
 })
-
+var psw1 = document.querySelector(".psw1")
+var psw2 = document.querySelector(".psw2")
+var chkpsw = document.querySelector(".chkpsw")
 
 function myFunction(){
-    var password = document.querySelectorAll(".psw")
 
-    for(var i of password){
+ 
 
-    if(i.type ==="password"){
-        i.type = "text";
+    if(psw1.type ==="password" && psw2.type==="password"){
+        psw1.type = "text";
+        psw2.type = "text";
     }
-    else(i.type = "password")
+    else{psw1.type = "password";
+    psw2.type = "password"}
 }
-}
+
+
+
+chkpsw.addEventListener("click", function(){
+
+    
+        if(psw1.value == psw2.value){
+            chkpsw.style.backgroundColor = "green"
+            chkpsw.style.color = "white"
+            chkpsw.innerHTML = "Confirmed"
+
+            console.log("kakhi")
+            
+        }
+        else{
+            chkpsw.style.backgroundColor = "green"
+            chkpsw.style.color = "white"
+            chkpsw.innerHTML = "Passwords do not match"
+            console.log("error")
+        }
+    }
+
+)
+
+
